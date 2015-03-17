6c7583a1ed191f1417be95684ec4dd4f26813ba8
Aanchal <aanchal@localhost.localdomain>
2015-03-17 19:57:23 -0400

    Using all html verbs

--- myApp/routes/index.js
+++ myApp/routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/post', function(req, res, next){
+	res.send('This is a post!');
+});
+
+router.put('/put', function(req, res, next){
+	res.send('This is a put!');
+});
+
+router.delete('/', function(req, res, next){
+	res.send('This is a update!');
+});
+
 module.exports = router;
